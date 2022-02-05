b882afeeb47bd7a5dbfa28c56116c27e63133242
Adrian Debasa <debasa1990@gmail.com>
2022-02-05 13:15:44 -0500

    constants and properties

--- src/main/java/com/ams/test/utilsGeneral/Constants.java
+++ src/main/java/com/ams/test/utilsGeneral/Constants.java
@@ -5,5 +5,7 @@ public class Constants {
     public static final String OK = "OK";
     public static final String KO = "KO";
     public final static String EXTENSION_PROPERTIES = ".properties";
+    public static final String APPLICATION_PROPERTY="application";
+
 
 }
