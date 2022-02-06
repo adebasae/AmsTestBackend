b07f85129f5fdabd676e44f1d6f1d5ce8dea95ff
Adrian Debasa <debasa1990@gmail.com>
2022-02-05 19:53:26 -0500

    remove unused import

--- src/main/java/com/ams/test/product/restcontroller/ProductDetailController.java
+++ src/main/java/com/ams/test/product/restcontroller/ProductDetailController.java
@@ -1,16 +1,12 @@
 package com.ams.test.product.restcontroller;
 
-import com.ams.test.product.beans.ProductDetail;
 import com.ams.test.product.restcontroller.interfaces.IProductDetailController;
 import com.ams.test.product.services.interfaces.IProductDetailService;
-import com.ams.test.utilsGeneral.Constants;
 import com.ams.test.utilsGeneral.Response.Pojo.BaseResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
-import java.util.ArrayList;
-
 
 @RestController
 public class ProductDetailController implements IProductDetailController {
